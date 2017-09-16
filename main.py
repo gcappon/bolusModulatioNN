a242096ac433e485fe1af90f6f0e38fb83551d6e
Giacomo Cappon <cappongiacomo@gmail.com>
2017-09-16 13:36:20 +0200

    Revert "20170916-0001"
    This reverts commit bec086ab0b1f235f5069f28e9cfe927e47da5fde.

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 #Loading useful packages
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 import os.path
 import sys
 import argparse
 
-#General purpose AI packages
-from sklearn.cross_validation import train_test_split,KFold
-
-#Keras packages
-from keras.models import Sequential
-from keras.layers import Dense, Activation
+#from keras.models import Sequential
+#from keras.layers import Dense, Activation
 
 ############## PARSING INPUT ###################################################
 class myArgumentParser(argparse.ArgumentParser):
@@ -46,46 +41,10 @@ OUTDIR = args.OUTDIR
 verbose = args.verbose
 ############## PARSING INPUT ###################################################
 
-############## PREPARING DATA ##################################################
+############## READING FILES ###################################################
 
 train = pd.read_table(DATAFILE,sep=',')
-train = np.asarray(train)
+print(train.describe())
 
 target = pd.read_table(TARGETFILE,sep=',')
-target = np.asarray(target)
-
-train_size = 0.7 #70% a training set e 30% a validation set
-X_tr, X_val, Y_tr, Y_val = train_test_split(train, target, train_size=train_size, random_state=0)
-print(np.shape(X_tr))
-print(np.shape(Y_tr))
-############## PREPARING DATA ##################################################
-
-############## BUILDING NN  ####################################################
-
-model = Sequential()
-model.add(Dense(units=20, input_dim=12))
-model.add(Activation('relu'))
-model.add(Dense(units=50))
-model.add(Activation('sigmoid'))
-model.add(Dense(units=50))
-model.add(Activation('sigmoid'))
-model.add(Dense(units=1))
-model.compile(loss='mean_squared_error',optimizer='adam')
-
-############## BUILDING NN  ####################################################
-
-############## TRAINING NN  ####################################################
-
-n_epochs = 1000
-model.fit(X_tr, Y_tr, epochs = n_epochs, batch_size = 100)
-
-############## TRAINING NN  ####################################################
-
-############## EVALUATING NN  ##################################################
-score = model.evaluate(X_val,Y_val,batch_size=100)
-Y_hat = model.predict(X_val)
-print('\n Score: ',score)
-
-plt.plot(Y_val[0:49], marker='^')
-plt.plot(Y_hat[0:49], marker='o')
-plt.show()
+print('Y',target.describe())
